40e50c56fdb6e21dce1e43867e323646a54c46cb
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-09-20 12:34:28 +0200

    access_right_test

--- test/models/access_right/access_right_test.dart
+++ test/models/access_right/access_right_test.dart
@@ -2,6 +2,13 @@ import 'package:accessboard_models/models/access_right/access_right.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  group('Base Class', () {
+    test('check if it is base class', () {
+      final NotAuthenticated notAuthenticated = NotAuthenticated();
+      expect(notAuthenticated, isA<AccessRightBase>());
+    });
+  });
+
   group('NotAuthenticated', () {
     test('fromJson', () {
       final json = {
