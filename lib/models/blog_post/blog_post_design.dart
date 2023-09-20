512c3ac188b33df5f243873f2e2939216fd19f3b
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-09-20 12:50:22 +0200

    update blog post model

--- lib/models/blog_post/blog_post_design.dart
+++ lib/models/blog_post/blog_post_design.dart
@@ -1,130 +1,109 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class BlogPostDesign {
   final Color? primaryColor;
   final Color? secondaryColor;
   final Color? tertiaryColor;
-  final BorderRadius? borderRadius;
-  final Border? border;
-  final Shadow? shadow;
+
+  final BorderRadiusEnum? borderRadiusEnum;
+  final BorderEnum? borderEnum;
+  final ShadowEnum? shadowEnum;
 
   BlogPostDesign({
     this.primaryColor,
     this.secondaryColor,
     this.tertiaryColor,
-    this.borderRadius,
-    this.border,
-    this.shadow,
+    this.borderRadiusEnum,
+    this.borderEnum,
+    this.shadowEnum,
   });
 
   factory BlogPostDesign.fromJson(Map<String, dynamic> json) {
     return BlogPostDesign(
-      primaryColor: json['primaryColor'] != null && json['primaryColor'] is int
-          ? Color(json['primaryColor'])
+      primaryColor: json['primaryColor'] != null
+          ? Color(json['primaryColor'] as int)
           : null,
-      secondaryColor:
-          json['secondaryColor'] != null && json['secondaryColor'] is int
-              ? Color(json['secondaryColor'])
-              : null,
-      tertiaryColor:
-          json['tertiaryColor'] != null && json['tertiaryColor'] is int
-              ? Color(json['tertiaryColor'])
-              : null,
-      borderRadius: _parseBorderRadius(json['borderRadius']),
-      border: _parseBorder(json['border']),
-      shadow: _parseShadow(json['shadow']),
+      secondaryColor: json['secondaryColor'] != null
+          ? Color(json['secondaryColor'] as int)
+          : null,
+      tertiaryColor: json['tertiaryColor'] != null
+          ? Color(json['tertiaryColor'] as int)
+          : null,
+      borderRadiusEnum: json['borderRadiusEnum'] != null
+          ? BorderRadiusEnum.values
+              .firstWhere((e) => describeEnum(e) == json['borderRadiusEnum'])
+          : null,
+      borderEnum: json['borderEnum'] != null
+          ? BorderEnum.values
+              .firstWhere((e) => describeEnum(e) == json['borderEnum'])
+          : null,
+      shadowEnum: json['shadowEnum'] != null
+          ? ShadowEnum.values
+              .firstWhere((e) => describeEnum(e) == json['shadowEnum'])
+          : null,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'primaryColor': primaryColor is Color ? primaryColor!.value : null,
-      'secondaryColor': secondaryColor is Color ? secondaryColor!.value : null,
-      'tertiaryColor': tertiaryColor is Color ? tertiaryColor!.value : null,
-      'borderRadius': _serializeBorderRadius(borderRadius),
-      'border': _serializeBorder(border),
-      'shadow': _serializeShadow(shadow),
+      'primaryColor': primaryColor?.value,
+      'secondaryColor': secondaryColor?.value,
+      'tertiaryColor': tertiaryColor?.value,
+      'borderRadiusEnum':
+          borderRadiusEnum != null ? describeEnum(borderRadiusEnum!) : null,
+      'borderEnum': borderEnum != null ? describeEnum(borderEnum!) : null,
+      'shadowEnum': shadowEnum != null ? describeEnum(shadowEnum!) : null,
     };
   }
 
-  static BorderRadius? _parseBorderRadius(String? value) {
+  static BorderRadius? getBorderRadius(BorderRadiusEnum? value) {
     switch (value) {
-      case 'none':
-        return null;
-      case 'small':
+      case BorderRadiusEnum.none:
+        return BorderRadius.zero;
+      case BorderRadiusEnum.small:
         return BorderRadius.circular(4);
-      case 'medium':
+      case BorderRadiusEnum.medium:
         return BorderRadius.circular(8);
-      case 'large':
+      case BorderRadiusEnum.large:
         return BorderRadius.circular(16);
       default:
         return null;
     }
   }
 
-  static String? _serializeBorderRadius(BorderRadius? value) {
-    if (value == null) {
-      return 'none';
-    } else if (value == BorderRadius.circular(4)) {
-      return 'small';
-    } else if (value == BorderRadius.circular(8)) {
-      return 'medium';
-    } else if (value == BorderRadius.circular(16)) {
-      return 'large';
-    } else {
-      return null;
-    }
-  }
-
-  static Border? _parseBorder(String? value) {
+  static Border? getBorder(BorderEnum? value) {
     switch (value) {
-      case 'none':
+      case BorderEnum.none:
         return null;
-      case 'small':
+      case BorderEnum.small:
         return Border.all(width: 1, color: Colors.grey.shade300);
-      case 'medium':
+      case BorderEnum.medium:
         return Border.all(width: 2, color: Colors.grey.shade300);
-      case 'large':
+      case BorderEnum.large:
         return Border.all(width: 4, color: Colors.grey.shade300);
       default:
         return null;
     }
   }
 
-  static String? _serializeBorder(Border? value) {
-    if (value == null) {
-      return 'none';
-    } else if (value.top.width == 1 &&
-        value.top.color == Colors.grey.shade300) {
-      return 'small';
-    } else if (value.top.width == 2 &&
-        value.top.color == Colors.grey.shade300) {
-      return 'medium';
-    } else if (value.top.width == 4 &&
-        value.top.color == Colors.grey.shade300) {
-      return 'large';
-    } else {
-      return null;
-    }
-  }
-
-  static Shadow? _parseShadow(String? value) {
+  static Shadow? getShadow(ShadowEnum? value) {
     switch (value) {
-      case 'none':
+      case ShadowEnum.none:
         return null;
-      case 'small':
+      case ShadowEnum.small:
         return BoxShadow(
           color: Colors.grey.shade300,
           blurRadius: 4,
           offset: const Offset(0, 2),
         );
-      case 'medium':
+      case ShadowEnum.medium:
         return BoxShadow(
           color: Colors.grey.shade300,
           blurRadius: 8,
           offset: const Offset(0, 4),
         );
-      case 'large':
+      case ShadowEnum.large:
         return BoxShadow(
           color: Colors.grey.shade300,
           blurRadius: 16,
@@ -134,24 +113,25 @@ class BlogPostDesign {
         return null;
     }
   }
+}
 
-  static String? _serializeShadow(Shadow? value) {
-    if (value == null) {
-      return 'none';
-    } else if (value.blurRadius == 4 &&
-        value.color == Colors.grey.shade300 &&
-        value.offset == const Offset(0, 2)) {
-      return 'small';
-    } else if (value.blurRadius == 8 &&
-        value.color == Colors.grey.shade300 &&
-        value.offset == const Offset(0, 4)) {
-      return 'medium';
-    } else if (value.blurRadius == 16 &&
-        value.color == Colors.grey.shade300 &&
-        value.offset == const Offset(0, 8)) {
-      return 'large';
-    } else {
-      return null;
-    }
-  }
+enum BorderRadiusEnum {
+  none,
+  small,
+  medium,
+  large,
+}
+
+enum BorderEnum {
+  none,
+  small,
+  medium,
+  large,
+}
+
+enum ShadowEnum {
+  none,
+  small,
+  medium,
+  large,
 }
