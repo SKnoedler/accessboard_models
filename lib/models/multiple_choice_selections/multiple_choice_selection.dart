0d0b14c1a00a238d3e888e929410039ac0c7795b
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-10-02 07:42:39 +0200

    update models

---
+++ lib/models/multiple_choice_selections/multiple_choice_selection.dart
@@ -0,0 +1,64 @@
+// collection: multipleChoiceSelections
+class MultipleChoiceSelection {
+  final List<MultipleChoiceSelectionItem> selectionItems;
+  final DateTime updatedAt;
+
+  MultipleChoiceSelection({
+    required this.selectionItems,
+    required this.updatedAt,
+  });
+  factory MultipleChoiceSelection.fromJson(Map<String, dynamic> json) {
+    var multipleChoiceSelectionItemsList = json['selectionItems'] as List;
+    List<MultipleChoiceSelectionItem> multipleChoiceSelectionItems =
+        multipleChoiceSelectionItemsList
+            .map((item) => MultipleChoiceSelectionItem.fromJson(item))
+            .toList();
+
+    return MultipleChoiceSelection(
+      selectionItems: multipleChoiceSelectionItems,
+      updatedAt: DateTime.parse(json['updatedAt']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    List<Map<String, dynamic>> multipleChoiceSelectionItemsJson =
+        selectionItems.map((item) => item.toJson()).toList();
+
+    return {
+      'selectionItems': multipleChoiceSelectionItemsJson,
+      'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+}
+
+class MultipleChoiceSelectionItem {
+  final String multipleChoiceFeedbackId;
+  final String deviceId;
+  final String selectedAnswer;
+  final DateTime selectedAt;
+
+  MultipleChoiceSelectionItem({
+    required this.multipleChoiceFeedbackId,
+    required this.deviceId,
+    required this.selectedAnswer,
+    required this.selectedAt,
+  });
+
+  factory MultipleChoiceSelectionItem.fromJson(Map<String, dynamic> json) {
+    return MultipleChoiceSelectionItem(
+      multipleChoiceFeedbackId: json['multipleChoiceFeedbackId'] as String,
+      deviceId: json['deviceId'] as String,
+      selectedAnswer: json['selectedAnswer'] as String,
+      selectedAt: DateTime.parse(json['selectedAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'multipleChoiceFeedbackId': multipleChoiceFeedbackId,
+      'deviceId': deviceId,
+      'selectedAnswer': selectedAnswer,
+      'selectedAt': selectedAt.toIso8601String(),
+    };
+  }
+}
--- lib/models/poll_answer/poll_answer.dart
+++ lib/models/poll_answer/poll_answer.dart
@@ -1,20 +1,49 @@
 // collection: pollAnswers
-
 class PollAnswer {
+  final List<PollAnswerItem> pollAnswerItems;
+  final DateTime updatedAt;
+
+  PollAnswer({
+    required this.pollAnswerItems,
+    required this.updatedAt,
+  });
+  factory PollAnswer.fromJson(Map<String, dynamic> json) {
+    var answerItemsList = json['pollAnswerItems'] as List;
+    List<PollAnswerItem> answerItems =
+        answerItemsList.map((item) => PollAnswerItem.fromJson(item)).toList();
+
+    return PollAnswer(
+      pollAnswerItems: answerItems,
+      updatedAt: DateTime.parse(json['updatedAt']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    List<Map<String, dynamic>> answerItemsJson =
+        pollAnswerItems.map((item) => item.toJson()).toList();
+
+    return {
+      'pollAnswerItems': answerItemsJson,
+      'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+}
+
+class PollAnswerItem {
   final String pollId;
   final String deviceId;
   final String selectedPollChoiceId;
   final DateTime selectedAt;
 
-  PollAnswer({
+  PollAnswerItem({
     required this.pollId,
     required this.deviceId,
     required this.selectedPollChoiceId,
     required this.selectedAt,
   });
 
-  factory PollAnswer.fromJson(Map<String, dynamic> json) {
-    return PollAnswer(
+  factory PollAnswerItem.fromJson(Map<String, dynamic> json) {
+    return PollAnswerItem(
       pollId: json['pollId'] as String,
       deviceId: json['deviceId'] as String,
       selectedPollChoiceId: json['selectedPollChoiceId'] as String,
