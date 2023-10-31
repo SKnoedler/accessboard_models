d8e27d8d0c1fbd06ba76db99882b33ff2cb7d770
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-10-31 14:24:35 +0100

    details

---
+++ lib/models/details_page/details_page.dart
@@ -0,0 +1,57 @@
+import 'package:accessboard_models/models/details_page/page_meta.dart';
+
+class DetailsPage {
+  final String pageId;
+  final String htmlContent;
+  final String title;
+  final PageMeta meta;
+  final List<dynamic>? quillDocData;
+
+  DetailsPage({
+    required this.pageId,
+    required this.htmlContent,
+    required this.title,
+    required this.meta,
+    this.quillDocData,
+  });
+
+  factory DetailsPage.fromJson(Map<String, dynamic> json) {
+    return DetailsPage(
+      pageId: json['pageId'] as String,
+      htmlContent: json['htmlContent'] as String,
+      title: json['title'] as String,
+      meta: PageMeta.fromJson(json['meta']),
+      quillDocData: json['quillDocData'] as List<dynamic>?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'pageId': pageId,
+      'htmlContent': htmlContent,
+      'title': title,
+      'meta': meta.toJson(),
+      'quillDocData': quillDocData,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is DetailsPage &&
+        other.pageId == pageId &&
+        other.htmlContent == htmlContent &&
+        other.title == title &&
+        other.meta == meta &&
+        other.quillDocData == quillDocData;
+  }
+
+  @override
+  int get hashCode =>
+      pageId.hashCode ^
+      htmlContent.hashCode ^
+      title.hashCode ^
+      meta.hashCode ^
+      quillDocData.hashCode;
+}
---
+++ lib/models/details_page/page_meta.dart
@@ -0,0 +1,30 @@
+class PageMeta {
+  final DateTime createdAt;
+  final DateTime? releasedAt;
+  final DateTime? updatedAt;
+
+  PageMeta({
+    required this.createdAt,
+    this.releasedAt,
+    this.updatedAt,
+  });
+
+  factory PageMeta.fromJson(Map<String, dynamic> json) {
+    return PageMeta(
+      createdAt: DateTime.parse(json['createdAt']),
+      releasedAt: json['releasedAt'] != null
+          ? DateTime.parse(json['releasedAt'])
+          : null,
+      updatedAt:
+          json['updatedAt'] != null ? DateTime.parse(json['updatedAt']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'createdAt': createdAt.toIso8601String(),
+      'releasedAt': releasedAt?.toIso8601String(),
+      'updatedAt': updatedAt?.toIso8601String(),
+    };
+  }
+}
