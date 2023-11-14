af0380ab3367913d6f2941d62aab0d5e32e6185e
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-11-14 17:06:19 +0100

    dd

--- lib/src/models/faq/faq.dart
+++ lib/src/models/faq/faq.dart
@@ -1,15 +1,18 @@
 import 'package:accessboard_models/src/models/faq/faq_item.dart';
 import 'package:accessboard_models/src/models/faq/faq_meta.dart';
+import 'package:accessboard_models/src/models/localized_string/localized_string.dart';
 
 class FAQ {
   final String faqId;
   final List<FaqItem> faqItems;
+  final LocalizedString? openFaqQuestion;
   final FaqMeta meta;
 
   FAQ({
     required this.faqId,
     required this.faqItems,
     required this.meta,
+    this.openFaqQuestion,
   });
 
   factory FAQ.fromJson(Map<String, dynamic> json) {
@@ -18,6 +21,9 @@ class FAQ {
         faqItemsJson.map((item) => FaqItem.fromJson(item)).toList();
 
     return FAQ(
+      openFaqQuestion: json['openFaqQuestion'] != null
+          ? LocalizedString.fromJson(json['openFaqQuestion'])
+          : null,
       faqId: json['faqId'] as String,
       faqItems: faqItems,
       meta: FaqMeta.fromJson(json['meta']),
@@ -26,6 +32,8 @@ class FAQ {
 
   Map<String, dynamic> toJson() {
     return {
+      'openFaqQuestion':
+          openFaqQuestion != null ? openFaqQuestion!.toJson() : null,
       'faqId': faqId,
       'faqItems': faqItems.map((item) => item.toJson()).toList(),
       'meta': meta.toJson(),
@@ -36,8 +44,10 @@ class FAQ {
     String? faqId,
     List<FaqItem>? faqItems,
     FaqMeta? meta,
+    LocalizedString? openFaqQuestion,
   }) {
     return FAQ(
+      openFaqQuestion: openFaqQuestion ?? this.openFaqQuestion,
       faqId: faqId ?? this.faqId,
       faqItems: faqItems ?? this.faqItems,
       meta: meta ?? this.meta,
@@ -50,10 +60,15 @@ class FAQ {
 
     return other is FAQ &&
         other.faqId == faqId &&
+        other.openFaqQuestion == openFaqQuestion &&
         other.faqItems == faqItems &&
         other.meta == meta;
   }
 
   @override
-  int get hashCode => faqId.hashCode ^ faqItems.hashCode ^ meta.hashCode;
+  int get hashCode =>
+      openFaqQuestion.hashCode ^
+      faqId.hashCode ^
+      faqItems.hashCode ^
+      meta.hashCode;
 }
