6fbbc3ae3c9e2c578a5d28556ed124490258a97d
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-11-17 16:50:20 +0100

    dd

--- lib/src/models/poll_answer/poll_answer.dart
+++ lib/src/models/poll_answer/poll_answer.dart
@@ -1,12 +1,14 @@
 // collection: pollAnswers
 class PollAnswer {
   final String pollId;
+  final String pollAnswerId;
   final String deviceId;
   final String selectedPollChoiceId;
   final DateTime selectedAt;
 
   PollAnswer({
     required this.pollId,
+    required this.pollAnswerId,
     required this.deviceId,
     required this.selectedPollChoiceId,
     required this.selectedAt,
@@ -15,6 +17,7 @@ class PollAnswer {
   factory PollAnswer.fromJson(Map<String, dynamic> json) {
     return PollAnswer(
       pollId: json['pollId'] as String,
+      pollAnswerId: json['pollAnswerId'] as String,
       deviceId: json['deviceId'] as String,
       selectedPollChoiceId: json['selectedPollChoiceId'] as String,
       selectedAt: DateTime.parse(json['selectedAt'] as String),
@@ -24,6 +27,7 @@ class PollAnswer {
   Map<String, dynamic> toJson() {
     return {
       'pollId': pollId,
+      'pollAnswerId': pollAnswerId,
       'deviceId': deviceId,
       'selectedPollChoiceId': selectedPollChoiceId,
       'selectedAt': selectedAt.toIso8601String(),
