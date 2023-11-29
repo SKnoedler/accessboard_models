14fc7d416be4be9845cd2302507bed3812332622
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-11-29 10:11:56 +0100

    update models

--- lib/src/models/design/custom_design.dart
+++ lib/src/models/design/custom_design.dart
@@ -1,12 +1,11 @@
 import 'package:accessboard_models/src/models/design/design_enums.dart';
-import 'package:flutter/material.dart';
 
 // this design class can be used in individual widgets
 
 class CustomDesign {
-  final Color? primaryColor;
-  final Color? secondaryColor;
-  final Color? tertiaryColor;
+  final int? primaryColor;
+  final int? secondaryColor;
+  final int? tertiaryColor;
 
   final BorderRadiusEnum? borderRadiusEnum;
   final BorderEnum? borderEnum;
@@ -23,15 +22,9 @@ class CustomDesign {
 
   factory CustomDesign.fromJson(Map<String, dynamic> json) {
     return CustomDesign(
-      primaryColor: json['primaryColor'] != null
-          ? Color(json['primaryColor'] as int)
-          : null,
-      secondaryColor: json['secondaryColor'] != null
-          ? Color(json['secondaryColor'] as int)
-          : null,
-      tertiaryColor: json['tertiaryColor'] != null
-          ? Color(json['tertiaryColor'] as int)
-          : null,
+      primaryColor: json['primaryColor'] as int?,
+      secondaryColor: json['secondaryColor'] as int?,
+      tertiaryColor: json['tertiaryColor'] as int?,
       borderRadiusEnum: json['borderRadiusEnum'] != null
           ? BorderRadiusEnum.values
               .firstWhere((e) => e.name == json['borderRadiusEnum'])
@@ -47,69 +40,12 @@ class CustomDesign {
 
   Map<String, dynamic> toJson() {
     return {
-      'primaryColor': primaryColor?.value,
-      'secondaryColor': secondaryColor?.value,
-      'tertiaryColor': tertiaryColor?.value,
+      'primaryColor': primaryColor,
+      'secondaryColor': secondaryColor,
+      'tertiaryColor': tertiaryColor,
       'borderRadiusEnum': borderRadiusEnum?.name,
       'borderEnum': borderEnum?.name,
       'shadowEnum': shadowEnum?.name,
     };
-  }
-
-  static BorderRadius? getBorderRadius(BorderRadiusEnum? value) {
-    switch (value) {
-      case BorderRadiusEnum.none:
-        return BorderRadius.zero;
-      case BorderRadiusEnum.small:
-        return BorderRadius.circular(4);
-      case BorderRadiusEnum.medium:
-        return BorderRadius.circular(8);
-      case BorderRadiusEnum.large:
-        return BorderRadius.circular(16);
-      default:
-        return null;
-    }
-  }
-
-  static Border? getBorder(BorderEnum? value) {
-    switch (value) {
-      case BorderEnum.none:
-        return null;
-      case BorderEnum.small:
-        return Border.all(width: 1, color: Colors.grey.shade300);
-      case BorderEnum.medium:
-        return Border.all(width: 2, color: Colors.grey.shade300);
-      case BorderEnum.large:
-        return Border.all(width: 4, color: Colors.grey.shade300);
-      default:
-        return null;
-    }
-  }
-
-  static Shadow? getShadow(ShadowEnum? value) {
-    switch (value) {
-      case ShadowEnum.none:
-        return null;
-      case ShadowEnum.small:
-        return BoxShadow(
-          color: Colors.grey.shade300,
-          blurRadius: 4,
-          offset: const Offset(0, 2),
-        );
-      case ShadowEnum.medium:
-        return BoxShadow(
-          color: Colors.grey.shade300,
-          blurRadius: 8,
-          offset: const Offset(0, 4),
-        );
-      case ShadowEnum.large:
-        return BoxShadow(
-          color: Colors.grey.shade300,
-          blurRadius: 16,
-          offset: const Offset(0, 8),
-        );
-      default:
-        return null;
-    }
   }
 }
--- lib/src/models/design/design.dart
+++ lib/src/models/design/design.dart
@@ -1,15 +1,14 @@
 import 'package:accessboard_models/src/models/design/design_enums.dart';
-import 'package:flutter/material.dart';
 
 // collection: designs
 
 class Design {
   final String designId;
 
-  final Color? backgroundColor;
-  final Color? primaryColor;
-  final Color? secondaryColor;
-  final Color? tertiaryColor;
+  final int? backgroundColor;
+  final int? primaryColor;
+  final int? secondaryColor;
+  final int? tertiaryColor;
 
   final BorderRadiusEnum? borderRadiusEnum;
   final BorderEnum? borderEnum;
@@ -29,18 +28,10 @@ class Design {
   factory Design.fromJson(Map<String, dynamic> json) {
     return Design(
       designId: json['designId'] as String,
-      backgroundColor: json['backgroundColor'] != null
-          ? Color(json['backgroundColor'] as int)
-          : null,
-      primaryColor: json['primaryColor'] != null
-          ? Color(json['primaryColor'] as int)
-          : null,
-      secondaryColor: json['secondaryColor'] != null
-          ? Color(json['secondaryColor'] as int)
-          : null,
-      tertiaryColor: json['tertiaryColor'] != null
-          ? Color(json['tertiaryColor'] as int)
-          : null,
+      backgroundColor: json['backgroundColor'] as int?,
+      primaryColor: json['primaryColor'] as int?,
+      secondaryColor: json['secondaryColor'] as int?,
+      tertiaryColor: json['tertiaryColor'] as int?,
       borderRadiusEnum: json['borderRadiusEnum'] != null
           ? BorderRadiusEnum.values
               .firstWhere((e) => e.name == json['borderRadiusEnum'])
@@ -57,70 +48,13 @@ class Design {
   Map<String, dynamic> toJson() {
     return {
       'designId': designId,
-      'backgroundColor': backgroundColor?.value,
-      'primaryColor': primaryColor?.value,
-      'secondaryColor': secondaryColor?.value,
-      'tertiaryColor': tertiaryColor?.value,
+      'backgroundColor': backgroundColor,
+      'primaryColor': primaryColor,
+      'secondaryColor': secondaryColor,
+      'tertiaryColor': tertiaryColor,
       'borderRadiusEnum': borderRadiusEnum?.name,
       'borderEnum': borderEnum?.name,
       'shadowEnum': shadowEnum?.name,
     };
-  }
-
-  static BorderRadius? getBorderRadius(BorderRadiusEnum? value) {
-    switch (value) {
-      case BorderRadiusEnum.none:
-        return BorderRadius.zero;
-      case BorderRadiusEnum.small:
-        return BorderRadius.circular(4);
-      case BorderRadiusEnum.medium:
-        return BorderRadius.circular(8);
-      case BorderRadiusEnum.large:
-        return BorderRadius.circular(16);
-      default:
-        return null;
-    }
-  }
-
-  static Border? getBorder(BorderEnum? value) {
-    switch (value) {
-      case BorderEnum.none:
-        return null;
-      case BorderEnum.small:
-        return Border.all(width: 1, color: Colors.grey.shade300);
-      case BorderEnum.medium:
-        return Border.all(width: 2, color: Colors.grey.shade300);
-      case BorderEnum.large:
-        return Border.all(width: 4, color: Colors.grey.shade300);
-      default:
-        return null;
-    }
-  }
-
-  static Shadow? getShadow(ShadowEnum? value) {
-    switch (value) {
-      case ShadowEnum.none:
-        return null;
-      case ShadowEnum.small:
-        return BoxShadow(
-          color: Colors.grey.shade300,
-          blurRadius: 4,
-          offset: const Offset(0, 2),
-        );
-      case ShadowEnum.medium:
-        return BoxShadow(
-          color: Colors.grey.shade300,
-          blurRadius: 8,
-          offset: const Offset(0, 4),
-        );
-      case ShadowEnum.large:
-        return BoxShadow(
-          color: Colors.grey.shade300,
-          blurRadius: 16,
-          offset: const Offset(0, 8),
-        );
-      default:
-        return null;
-    }
   }
 }
