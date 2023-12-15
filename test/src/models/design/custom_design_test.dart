bc7afe4bc7b2a14aad6cfe29e4192b0e39239611
SteffenKnoedler <121494955+SteffenKnoedler@users.noreply.github.com>
2023-12-15 12:41:52 +0100

    okok

--- test/src/models/design/custom_design_test.dart
+++ test/src/models/design/custom_design_test.dart
@@ -1,36 +1,36 @@
+import 'package:accessboard_models/src/models/design/custom_design.dart';
 import 'package:accessboard_models/src/models/design/design_enums.dart';
-import 'package:accessboard_models/src/models/design/flutter_custom_design.dart';
-import 'package:flutter/material.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final expectedObject = FlutterCustomDesign(
-    primaryColor: Colors.red,
-    secondaryColor: Colors.blue,
-    tertiaryColor: Colors.red,
+  final expectedObject = DartCustomDesign(
+    primaryColor: 4294198070,
+    secondaryColor: 4280391411,
+    tertiaryColor: 4294961979,
     borderRadiusEnum: BorderRadiusEnum.small,
     borderEnum: BorderEnum.medium,
     shadowEnum: ShadowEnum.none,
   );
 
   final expectedJson = {
-    'primaryColor': 123,
-    'secondaryColor': 456,
-    'tertiaryColor': 789,
+    'primaryColor': 4294198070,
+    'secondaryColor': 4280391411,
+    'tertiaryColor': 4294961979,
     'borderRadiusEnum': 'small',
     'borderEnum': 'medium',
-    'shadowEnum': 'none',
+    'shadowEnum': 'none'
   };
 
   group('CustomDesign', () {
     test('fromJson() should correctly deserialize JSON', () {
-      final result = FlutterCustomDesign.fromJson(expectedJson);
+      final result = DartCustomDesign.fromJson(expectedJson);
 
       expect(result, expectedObject);
     });
 
     test('toJson() should correctly serialize to JSON', () {
       final result = expectedObject.toJson();
+      print(result);
 
       expect(result, expectedJson);
     });
